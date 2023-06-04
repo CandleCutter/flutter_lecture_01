eae148bc57efbe0580c2693976bd6be73b91a1ba
CandleCutter <dejavuguy@naver.com>
2023-06-04 16:36:03 +0900

    함수 추가

---
+++ lib/ch1/03.method.dart
@@ -0,0 +1,61 @@
+class Method {
+
+  Method() {
+    /// int age = add();
+    /// add() 메소드는 리턴값이 없으니 에러가 남
+    int age = add2();
+    print('Method.Method age : $age');
+
+    double result = calculate(10, 20);
+    print('Method.Method calculate : $result');
+    print('Method.Method calculate2 : ${calculate(20, 120)}');
+    print('Method.Method calculate3: ${calculate(210, 511)}');
+    print('Method.Method calculate4 : ${calculate(2220, 410)}');
+    print('Method.Method calculate5 : ${calculate(510, 230)}');
+
+    String kk = introduce(name: "송우영", name2: "송우영2");
+    print('Method.Method kk: $kk');
+
+    optional("송우영");
+    var ooo = optional("송우영",b:"안써도 되는 것");
+    var ooo1 = optional("송우영");
+    print('Method.Method ooo: $ooo');
+    print('Method.Method ooo: $ooo1');
+
+
+  }
+
+  /// void = 리턴 타입이 없다
+  /// 리턴타입  함수 이름  구현부
+  /// int     add()     {}
+  void add() {
+
+  }
+
+  /// add2 메소드는 리턴을 int형으로 30을 줌
+  int add2() {
+    return 30;
+  }
+
+  /// 계산합수
+  /// 두 숫자를 받아 값을더하고 2로 나눈뒤 1.5를 곱한 값을 리턴
+  double calculate(int a, int b) {
+    return ((a + b) / 2) * 2.5;
+  }
+
+  /// String type 함수 만들어 쓰기
+  String introduce({
+    required String name,
+    required String name2,
+
+  }) {
+    return "안녕하세요. $name입니다.";
+
+    /// String type 함수 만들어 쓰기
+
+  }
+  String optional(String a, {String b="빈 값"}){
+    return"$a, $b, 잘부탁드립니다";
+  }
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lecture_01/ch1/01.variable.dart';
 import 'package:flutter_lecture_01/ch1/02.operator.dart';
+import 'package:flutter_lecture_01/ch1/03.method.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_lecture_01/main.dart';
 
 void main() {
 
   Variable();
-
+  print("----------------");
   Operator();
+  print("----------------");
+  Method();
 
 }

8e1793593f103431d19abc3cef901888bebb5d12
CandleCutter <dejavuguy@naver.com>
2023-06-11 14:27:04 +0900

    ch2 클래스 생성

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@ import 'package:flutter_lecture_01/ch1/04.conditional.dart';
 import 'package:flutter_lecture_01/ch1/05.collections.dart';
 import 'package:flutter_lecture_01/ch1/06.for_loop.dart';
 import 'package:flutter_lecture_01/ch2/01.person.dart';
+import 'package:flutter_lecture_01/ch2/02.product.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_lecture_01/main.dart';
 
@@ -25,14 +26,24 @@ void main() {
   //사용자가 타입을 만들수 있음
 
   /// 클래스를 생성할 때는 ()를 써줘야 함.
-  Person person = Person("김진환",34,78.0);
-  person.talk();
+  // Person person = Person("김진환",34,78.0);
+  // person.talk();
+  //
+  // Person person1 = Person("김진환1",341,178.0);
+  // person1.talk();
+  //
+  // Person person2 = Person("김진환2",3411,1178.0);
+  // person2.talk();
 
-  Person person1 = Person("김진환1",341,178.0);
-  person1.talk();
+  /// 더조은 회사의 선풍기를 몇대 팔았는지 확인
+  Product a = Product("선풍기", "더조은");
+  a.sale();
+  a.sale();
+  a.sale();
+  a.sum();
 
-  Person person2 = Person("김진환2",3411,1178.0);
-  person2.talk();
+
+
 
 
 

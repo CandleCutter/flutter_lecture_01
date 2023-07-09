7f4c9ab67656d0f644c9d1fa82f829d79bc4badf
CandleCutter <dejavuguy@naver.com>
2023-07-09 10:06:52 +0900

    opacity update

--- lib/ch5/01.pageview_screen.dart
+++ lib/ch5/01.pageview_screen.dart
@@ -8,28 +8,68 @@ class PageViewScreen extends StatefulWidget {
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
+
+  final PageController pageController = PageController();
+
+  double opacity = 1;
+
+  @override
+  void initState() {
+    ///addListener는 pageview가 동작할 때마다 호출
+    pageController.addListener(() {
+
+      if (pageController.page!<1){
+        setState(() {
+          opacity = pageController.page!;
+        });
+      }
+      print(pageController.page);
+    });
+    super.initState();
+  }
+
+  /// 화면이 종료될 때 호출
+  @override
+  void dispose(){
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("PageViewScreen"),
       ),
-      body: PageView(
-          onPageChanged: (value) {
-            print("value : $value");
-          },
-          scrollDirection: Axis.vertical,
-          children: [
-            Container(
-              color: Colors.cyan,
-            ),
-            Container(
-              color: Colors.red,
-            ),
-            Container(
-              color: Colors.greenAccent,
-            ),
-          ]),
+      body: Column(
+        children: [
+          Opacity(opacity: opacity,
+          child: Container(
+            color: Colors.black,
+            height: 50,
+          ),
+          ),
+          Expanded(
+            child: PageView(
+                controller: pageController,
+                onPageChanged: (value) {
+                  print("value : $value");
+                },
+                // scrollDirection: Axis.vertical, ///default :  horizontal
+                children: [
+                  Container(
+                    color: Colors.red, // index : 0
+                  ),
+                  Container(
+                    color: Colors.green, // index : 1
+                  ),
+                  Container(
+                    color: Colors.brown, // index : 2
+                  ),
+                ]),
+          ),
+        ],
+      ),
     );
   }
 }
